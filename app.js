d740d2f0c5351aa3baf74ba1ef81d424bc57f957
Mahdi <mahdimahdi9731@gmail.com>
2023-12-20 17:25:40 +0330

    improve security: Setting Security HTTP Headers, Data Sanitization, Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ import userRouter from "./routes/userRoutes.js";
 import AppError from "./utils/appError.js";
 import { errorController } from "./controllers/errorController.js";
 import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
 
 export const app = express();
+
+app.use(helmet());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -14,7 +20,16 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
 
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/users", userRouter);
